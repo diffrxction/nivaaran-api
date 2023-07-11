dbac8744db6e50c8bbac9572785cd4a448a2b836
RownakM <rmazumder1033@gmail.com>
2023-07-11 11:12:17 +0530

    URL changed

--- nivaaransite/settings.py
+++ nivaaransite/settings.py
@@ -149,4 +149,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-PROJECT_URL = "http://localhost:8000"
+PROJECT_URL = "http://13.126.228.253/"
